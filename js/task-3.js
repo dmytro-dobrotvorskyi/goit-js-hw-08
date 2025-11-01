7f54d88a4e11dd5a46964c0e4e6ff63a0d0e305b
Dmytro Dobrotvorskyi <ddobrotvorskyi@gmail.com>
2025-11-01 20:21:48 +0200

    hw-8

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const inputForm = document.querySelector('#name-input');
-const output = document.querySelector('#name-output');
-
-inputForm.addEventListener('input', event => {
-  const value = event.currentTarget.value.trim();
-  output.textContent = value ? value : 'Anonymous';
-});
